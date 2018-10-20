3c64a3b826ff251adc56e702fa89d5f8c2a0784a
Meredith Avery <quassibly@gmail.com>
2018-10-20 14:47:56 +0200

    fixed tagbar and navbar helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def current_category?(test_path)
+    return 'navbar-item navbar-link active' if request.path == test_path
+    'navbar-item navbar-link'
+  end
+
+  def current_tag?(current_tag)
+    return 'tagbar-link active' if params[:tag] == current_tag
+    'tagbar-link'
+  end
+
 end
