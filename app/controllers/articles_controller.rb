e3af1da04295568fdbd40cc0606095d35cff2f8d
Meredith Avery <quassibly@gmail.com>
2018-10-31 14:33:26 +0100

    created article_path to redirect to correct show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.where(published: true)
   end
+
+  def show
+    @article = Article.find(params[:id])
+    if @article.category == "profile"
+      redirect_to profile_path(@article)
+    else @article.category == "opinion"
+      redirect_to opinion_path(@article)
+    end
+  end
 end
