4c14d1f4a32e3f16be6e46999d99b882c0d40663
Meredith Avery <quassibly@gmail.com>
2019-01-22 12:34:33 +0100

    added podcast views

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,28 +1,44 @@
 class PodcastsController < ApplicationController
-  before_action :find_media
-  before_action :set_type
+  # before_action :find_media
+  before_action :set_category
 
   def index
     @page = 'index'
-    @media = Media.where(type: 'podcast').where(deleted: false)
+    @podcasts = Article.where(category: 'podcast').where(deleted: false)
     if params[:tag].present?
-      @media = @media.where(tag: params[:id])
+      @podcasts = @podcasts.where(tag: params[:id])
     end
-    @media = @media.sort_by &:updated_at
-    @media.reverse!
+    @podcasts = @podcasts.sort_by &:updated_at
+    @podcasts.reverse!
   end
 
   def show
     @page = 'grey'
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @page = 'grey'
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(podcast_params)
+    redirect_to podcasts_path
   end
 
   private
 
-  def find_media
-    @media = Media.find(params[:id])
+  def set_category
+    @category = 'podcast'
   end
 
-  def set_type
-    @type = 'podcast'
+  def podcast_params
+    params.require(:article).permit(:headline, :subhead, :tag, :photo, :body, :published, :deleted, :category, :seo_title, :meta, :feature, :url)
   end
 end
