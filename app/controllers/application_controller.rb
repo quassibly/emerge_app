70655a57db4693019254b1f3a45ed155bffbed85
Meredith Avery <quassibly@gmail.com>
2019-02-17 20:52:31 +0100

    rich metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:show, :index]
+
+  def default_url_options
+    { host: ENV['HOST'] || 'localhost:3000' }
+  end
 end
