36eb2dd467a25f5fc4b8bd967c42202df6cd3d5b
Meredith Avery <quassibly@gmail.com>
2019-02-27 13:40:27 +0100

    photo redirect

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,7 +29,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save!
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to photo_path(@photo), notice: 'Photo was successfully created.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
