2f773f317f4ef3511b3228bec0bcaeb064ad0660
Meredith Avery <quassibly@gmail.com>
2020-05-06 18:01:30 +0200

    protect from missing contributors

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,5 +1,5 @@
 module OpinionsHelper
   def opinions_photo
-    @article.photo.present? ? @article.photo : @article.contributor.photo
+    @article.photo.present? ? @article.photo : @article.contributor&.photo
   end
 end
