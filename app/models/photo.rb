bfd1e3d0f176d4517bade8a8d5416e99fab3a6ff
Meredith Avery <quassibly@gmail.com>
2020-05-25 23:16:40 +0200

    fix shrine

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,3 @@
-class Photo < ApplicationRecord
-  include ImageUploader[:image]
+class Photo < ActiveRecord::Base
+  include ImageUploader::Attachment(:image) # adds an `image` virtual attribute
 end
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -13,13 +13,4 @@ Shrine.storages = {
   store: Shrine::Storage::Cloudinary.new(prefix: "store"),
 }
 
-# Shrine.storages = {
-#   # temporary storage
-#   cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
-
-#   # permanent storage
-#   store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'),
-# }
-
-# Shrine.plugin :activerecord
-# Shrine.plugin :cached_attachment_data # for forms
+Shrine.plugin :activerecord
